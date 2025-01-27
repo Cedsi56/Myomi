658a416af0f9ec890eefc69ae2221e82a4f484e5
Cedsi <cedsitails@gmail.com>
2025-01-27 21:17:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -739,7 +739,7 @@ async def play(ctx, *, link):
         print("bb")
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(link, download=True))
         print(data)
-        my_data = data["requested_downloads"][0]["requested_formats"]
+        my_data = data["requested_downloads"][0]["filepath"]
         print(f"----------- {my_data}")
         player = discord.FFmpegOpusAudio(data, **ffmpeg_options)
 
